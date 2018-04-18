9f011986a5e799d5afd7444632125a115f6d8f7e
zabellfour <zabell2814@gmail.com>
2018-04-19 00:55:24 +0300

    added header

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,21 @@
-// You can write a call and import your functions in this file.
-//
-// This file will be compiled into app.js and will not be minified.
-// Feel free with using ES6 here.
 
-import DE from './modules/dots';
 
 ( ($) => {
   'use strict';
 
   // When DOM is ready
   $(() => {
-    DE.dotsEffect();
+    //mob Nav
+    function mobNav(opener){
+      let $opener = $(opener);
+      let $body = $('body');
+      $opener.on('click', function(){
+        $body.toggleClass('nav-active');
+      });
+    }
+
+    mobNav('.menu-opener');
+    
   });
 
 })(jQuery);
