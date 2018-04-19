62dc4baa944d00f3fdf0dc774db17fabffb808da
zabellfour <zabell2814@gmail.com>
2018-04-20 00:42:34 +0300

    added banner section and structure of dlider

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,47 @@
+(($) => {
+    'use strict';
 
-
-( ($) => {
-  'use strict';
-
-  // When DOM is ready
-  $(() => {
-    //mob Nav
-    function mobNav(opener){
-      let $opener = $(opener);
-      let $body = $('body');
-      $opener.on('click', function(){
-        $body.toggleClass('nav-active');
-      });
-    }
-
-    mobNav('.menu-opener');
-    
-  });
+    // When DOM is ready
+    $(() => {
+        //mob Nav
+        function mobNav(opener, close) {
+            let $opener = $(opener);
+            let $close = $(close);
+            let $body = $('body');
+            $opener.on('click', function() {
+                $body.toggleClass('nav-active');
+            });
+            $close.on('click', function() {
+                $body.toggleClass('nav-active');
+            });
+        }
+        var scroll = new SmoothScroll('[data-scroll]');
+        mobNav('.menu-opener', '.menu-close');
+        $(".events-slider").owlCarousel({
+            margin: 120,
+            items: 1,
+            stagePadding: 500,
+            loop: true,
+            responsive: {
+                0: {
+                    items: 1,
+                    stagePadding: 150,
+                    margin: 30
+                },
+                1024: {
+                    stagePadding: 250,
+                    margin: 60
+                },
+                1200: {
+                    stagePadding: 400,
+                    margin: 120
+                },
+                1500: {
+                    margin: 120,
+                    stagePadding: 500
+                }
+            }
+        });
+    });
 
 })(jQuery);
